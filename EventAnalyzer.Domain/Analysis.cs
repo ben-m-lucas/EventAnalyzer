f487ee1c227a9f387cf3784643916e42a5e274ac
Ben Lucas <ben_m_lucas@yahoo.com>
2016-09-19 18:34:12 -0700

    initial implementation and design of domain objects

---
+++ EventAnalyzer.Domain/Analysis.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAnalyzer.Domain
+{
+    /// <summary>
+    /// An analysis of the provided events
+    /// </summary>
+    public class Analysis
+    {
+        public Analysis(IEnumerable<SwitchEvent> events)
+        {
+        }
+
+        /// <summary>
+        /// Total seconds from all of the events
+        /// </summary>
+        public int TotalSeconds
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// The number of events
+        /// </summary>
+        public int EventCount
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Average of all event seconds rounded to the nearest integer
+        /// </summary>
+        public int AverageSeconds
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// An array of all event seconds sorted from smallest to largest
+        /// </summary>
+        public int[] SortedSeconds
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// The Event Type that occurred most often
+        /// </summary>
+        public SwitchEventType MostCommonEventType
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ EventAnalyzer.Domain/SwitchEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace EventAnalyzer.Domain
+{
+    public class SwitchEvent
+    {
+        public int EventSeconds { get; set; }
+
+        public SwitchEventType EventType { get; set; }
+    }
+}
